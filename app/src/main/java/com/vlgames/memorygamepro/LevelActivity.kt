61fdfabddb3d2ab8151ab60b71f7ac8181e99127
Your Name <you@example.com>
2020-01-03 13:31:17 +0200

    getting back to backside but can't compare two img

---
+++ app/src/main/java/com/vlgames/memorygamepro/LevelActivity.kt
@@ -0,0 +1,114 @@
+package com.vlgames.memorygamepro
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.vlgames.memorygamepro.R.drawable.*
+import kotlinx.android.synthetic.main.activity_level.*
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class LevelActivity : AppCompatActivity() {
+
+
+    private var arrayImage: MutableList<Int> = mutableListOf()
+    lateinit var arrayButtons: Array<Button>
+    private val cardBack = hidden_card
+    var clicked = 0
+    var turnOver = false
+    var lastClicked = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level)
+
+        arrayImage = mutableListOf(
+            bike_1, bike_2, bike_3, bike_4, bike_5,
+            bike_6, bike_7, bike_8, bike_1, bike_2, bike_3, bike_4, bike_5, bike_6, bike_7, bike_8
+        )
+
+        arrayButtons = arrayOf(
+            button1, button2, button3, button4, button5, button6, button7, button8,
+            button9, button10, button11, button12, button13, button14, button15, button16
+        )
+
+
+
+        arrayImage.shuffle()
+        for (i in 0..15) {
+            clicked = 0
+            arrayButtons[i].tag
+            arrayButtons[i].textSize = 0.0F
+            arrayButtons[i].setText(arrayImage[i])
+            arrayButtons[i].setOnClickListener {
+                if (clicked == 2) {
+                    turnOver = true
+                    if (arrayButtons[i].text == arrayButtons[i].tag && !turnOver) {
+                        arrayButtons[i].isClickable = false
+                        arrayButtons[lastClicked].isClickable = false
+                        turnOver = false
+                    }
+                } else {
+                    arrayButtons[i].setBackgroundResource(arrayImage[i])
+                    if (clicked == 0) {
+                        lastClicked = i
+                    }
+                    clicked++
+                    if (clicked == 2) {
+                        timerTurnOver(arrayButtons[i], arrayButtons[lastClicked])
+                    }
+                }
+                Log.d("Clicked", arrayImage[i].toString())
+
+            }
+        }
+    }
+//        for (i in 0..15) {
+//            arrayButtons[i].textSize = 0.0F
+//            arrayButtons[i].setText(image[i])
+//            arrayButtons[i].setOnClickListener {
+//                if (clicked == 0) {
+//                    lastClicked = i
+//                }
+//                if (clicked == 2) {
+//                    if (arrayButtons[i].text == arrayButtons[lastClicked].text) {
+//                        arrayButtons[i].isClickable = false
+//                        arrayButtons[lastClicked].isClickable = false
+//                        turnOver = false
+//                    } else {
+//                        timerTurnOver(arrayButtons[i], arrayButtons[lastClicked])
+//                        clicked = 0
+//                        clicked++
+//                    }
+//
+//                } else {
+//                    arrayButtons[i].setBackgroundResource(image[i])
+//                }
+//
+//                Log.d("Clicked", clicked.toString())
+//            }
+//        }
+//    }
+
+    private fun timerTurnOver(i: View, lastClicked: View) {
+        Timer().schedule(timerTask {
+            flipOverCards(i, lastClicked as TextView)
+        }, 1000)
+    }
+
+    private fun flipOverCards(i: View, lastClicked: View) {
+        i.setBackgroundResource(cardBack)
+        lastClicked.setBackgroundResource(cardBack)
+        clicked = 0
+    }
+
+
+}
--- app/src/main/java/com/vlgames/memorygamepro/MainActivity.kt
+++ app/src/main/java/com/vlgames/memorygamepro/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.vlgames.memorygamepro
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vlgames.memorygamepro.R.drawable.*
@@ -11,55 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image: MutableList<Int> = mutableListOf(
-            bike_1, bike_2, bike_3, bike_4, bike_5, bike_6,
-            bike_7, bike_8, bike_1, bike_2, bike_3, bike_4, bike_5, bike_6,
-            bike_7, bike_8
-        )
+        start_btn.setOnClickListener {
+            val intent = Intent(this, LevelActivity::class.java)
+            startActivity(intent)
+        }
 
-        val buttons = arrayOf(
-            button1, button2, button3, button4, button5, button6, button7, button8,
-            button9, button10, button11, button12, button13, button14, button15, button16
-        )
-
-        val cardBack = hidden_card
-        var clicked = 0
-        var turnOver = false
-        var lastClicked = -1
-
-        image.shuffle()
-        for (i in 0..15) {
-//            buttons[i].setBackgroundResource(bike_1)
-            buttons[i].text = "cardBack"
-            buttons[i].textSize = 0.0F
-            buttons[i].setOnClickListener {
-                if (buttons[i].text == "cardBack" && !turnOver) {
-                    buttons[i].setBackgroundResource(image[i])
-                    buttons[i].setText(image[i])
-                    if (clicked == 0) {
-                        lastClicked = i
-                    }
-                    clicked++
-
-                } else if (buttons[i].text !in "cardBack") {
-                    buttons[i].setBackgroundResource(cardBack)
-                    buttons[i].text = "cardBack"
-                    clicked--
-                }
-
-
-                if (clicked == 2) {
-                    turnOver = true
-                    if (buttons[i].text == buttons[lastClicked].text) {
-                        buttons[i].isClickable = false
-                        buttons[lastClicked].isClickable = false
-                        turnOver = false
-                        clicked = 0
-                    }
-                } else if (clicked == 0) {
-                    turnOver = false
-                }
-            }
+        exit_btn.setOnClickListener {
+            finishAffinity()
         }
     }
 }
